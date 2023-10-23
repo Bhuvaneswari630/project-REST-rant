287074b7164df186d0bccee16117f8b701a0709a
bhuvaneswari630 <bhuvaneswarim630@gmail.com>
2023-10-23 10:50:54 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express')
+const PORT = process.env.PORT
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hompage')
+})
+
+app.listen(PORT, () => {
+    console.log('listening on port', PORT)
+})
