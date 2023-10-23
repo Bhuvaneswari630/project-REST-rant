161e632ccff436b0d732a4ee7c15dba1edef93d5
bhuvaneswari630 <bhuvaneswarim630@gmail.com>
2023-10-23 11:00:15 -0400

    Adding 404 page route

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('Hompage')
 })
+app.get('*', (req, res) => {
+    res.status(404).send(`<h1>404 page</h1>`)
+})
 
 app.listen(PORT, () => {
     console.log('listening on port', PORT)
