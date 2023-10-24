ad0443bcb809a4866a5d5f91bc33e7a2c968d302
bhuvaneswari630 <bhuvaneswarim630@gmail.com>
2023-10-24 16:51:18 -0400

    Adding default view for layout

--- controllers/places.js
+++ controllers/places.js
@@ -1,8 +1,33 @@
 const router = require('express').Router()
 
 
+
+router.get('/new', (req, res) => {
+    res.send('Form page for creating a new place')
+})
+router.get('/:id', (req, res) => {
+    res.send('Details about a particular place')
+})
+router.put('/:id', (req, res) => {
+    res.send('Update a particular place')
+})
+router.delete('/:id', (req, res) => {
+    res.send('Delete a particular place')
+})
+router.get('/:id/edit', (req, res) => {
+    res.send('Form page for editing an existing place')
+})
+router.post('/:id/rant', (req, res) => {
+    res.send('Create a rant (comment) about a particular place')
+})
+router.delete('/:id/:rantId', (req, res) => {
+    res.send('Delete a rant (comment) about a particular place')
+})
 router.get('/', (req, res) => {
     res.send('GET /places')
+})
+router.post('/', (req, res) => {
+    res.send('Create new place')
 })
 
 module.exports = router
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hompage')
+    res.send('Home page')
 })
 app.get('*', (req, res) => {
     res.status(404).send(`<h1>404 page</h1>`)
