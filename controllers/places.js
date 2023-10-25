906cbffff99538131b0236aeb2907af15161003d
bhuvaneswari630 <bhuvaneswarim630@gmail.com>
2023-10-25 16:36:35 -0400

    Adding show place view

--- controllers/places.js
+++ controllers/places.js
@@ -1,10 +1,24 @@
 const router = require('express').Router()
-
+let places = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: 'http://placekitten.com/250/250'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: 'http://placekitten.com/250/250'
+  }]
 router.get('/new', (req, res) => {
     res.send('Form page for creating a new place')
 })
 router.get('/:id', (req, res) => {
-    res.send('Details about a particular place')
+    let place = places[req.params.id]
+    res.render('places/Show', { place })
+    // res.send('Details about a particular place')
 })
 router.put('/:id', (req, res) => {
     res.send('Update a particular place')
@@ -22,21 +36,7 @@ router.delete('/:id/:rantId', (req, res) => {
     res.send('Delete a rant (comment) about a particular place')
 })
 router.get('/', (req, res) => {
-    // res.send('GET /places')
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-      }]
-      
+    // res.send('GET /places')  
     res.render('places/Index', { places })
 })
 router.post('/', (req, res) => {
--- views/places/Index.jsx
+++ views/places/Index.jsx
@@ -2,18 +2,19 @@ const React = require('react')
 const Default = require('../Default')
 
 function Index({ places }) {
+    let placesFormatted = places.map((place, index) => {
+        return (
+            <div key={index}>
+                <h2>{place.name}</h2>
+                <img src={place.pic} alt={place.name} />
+            </div>
+        )
+    })
     return (
         <Default>
             <main>
                 <h1>Places Index Page</h1>
-                {places.map((place, index) => {
-                    return (
-                        <div key={index}>
-                            <h2>{place.name}</h2>
-                            <img src={place.pic} alt={place.name} />
-                        </div>
-                    )
-                })}
+                {placesFormatted}
             </main>
         </Default>
     )
