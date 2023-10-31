66c72ea2179332ad2c749fa998d2188ac099d6ce
bhuvaneswari630 <bhuvaneswarim630@gmail.com>
2023-10-30 21:09:43 -0400

    Adding edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -27,6 +27,31 @@ router.get('/new', (req, res) => {
     // res.send('Form page for creating a new place')
     res.render('places/New')
 })
+router.put('/:id', (req, res) => {
+    // res.send('Update a particular place')
+    let index = Number(req.params.id)
+    console.log(req.body);
+    if (isNaN(index)) {
+        res.render('Error404')
+    } else if (!places[index]) {
+        res.render('Error404')
+    } else {
+        let place = places[index]
+        // if (!req.body.pic) {
+        //     // Default image if not provided
+        //     place.pic = 'http://placekitten.com/400/400'
+        // }
+        // if (!req.body.city) {
+        //     place.city = 'Anytown'
+        // }
+        // if (!req.body.state) {
+        //     place.state = 'USA'
+        // }
+        // places[index] = req.body
+        places[index] = { ...place, ...req.body }
+        res.redirect(`/places/${index}`)
+    }
+})
 router.delete('/:id', (req, res) => {
     let index = Number(req.params.id)
     if (isNaN(index)) {
@@ -40,14 +65,7 @@ router.delete('/:id', (req, res) => {
 
     // res.send('Delete a particular place')
 })
-router.put('/:id', (req, res) => {
-    // res.send('Update a particular place')
-    let index = req.params.id
-    places[index] = req.body
-    // console.log(req.body);
-    places[index] = { ...places[index], ...req.body }
-    res.redirect(`/places/${index}`)
-})
+
 router.get('/:id', (req, res) => {
     let index = Number(req.params.id)
     if (isNaN(index)) {
@@ -62,7 +80,7 @@ router.get('/:id', (req, res) => {
             index: index
         })
     }
-    
+
     // res.send('Details about a particular place')
 })
 
@@ -75,7 +93,7 @@ router.get('/:id/edit', (req, res) => {
         res.render('Error404')
     } else {
         let place = places[req.params.id]
-        res.render('places/Edit', { 
+        res.render('places/Edit', {
             place: place,
             index: index
         })
--- views/places/Edit.jsx
+++ views/places/Edit.jsx
@@ -2,16 +2,19 @@ const React = require('react')
 const Default = require('../Default')
 
 function Edit({ place, index }) {
+    console.log('index', index)
     return (
         <Default>
             <main>
                 <h1>Edit Place</h1>
-                <form action={`/places/${index}?_method=PUT`} method='POST' >
+                <form action={`/places/${index}?_method=PUT`} method='POST'>
                     <div className="row m-1">
                         <div className='form-group col-6'>
                             <label htmlFor="name" >Name</label>
                             <input
                                 type="text"
+                                id='name'
+                                name='name'
                                 className="form-control"
                                 defaultValue={place.name}
                             />
@@ -20,16 +23,20 @@ function Edit({ place, index }) {
                             <label htmlFor="city">City</label>
                             <input
                                 type="text"
+                                id='city'
+                                name='city'
                                 className="form-control"
                                 defaultValue={place.city}
                             />
                         </div>
                     </div>
                     <div className="row m-1">
-                    <div className='form-group col-6'>
+                        <div className='form-group col-6'>
                             <label htmlFor="state">State</label>
                             <input
                                 type="text"
+                                name='state'
+                                id='state'
                                 className="form-control"
                                 defaultValue={place.state}
                             />
@@ -38,6 +45,8 @@ function Edit({ place, index }) {
                             <label htmlFor="cuisines">Cuisines</label>
                             <input
                                 type="text"
+                                name='cuisines'
+                                id='cuisines'
                                 className="form-control"
                                 defaultValue={place.cuisines}
                             />
@@ -48,6 +57,8 @@ function Edit({ place, index }) {
                         <label htmlFor="pic">Image URL</label>
                         <input
                             type="text"
+                            name='pic'
+                            id='pice'
                             className="form-control"
                             defaultValue={place.pic}
                         />
